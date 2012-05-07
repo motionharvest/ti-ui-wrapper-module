146a3f6e96f807e5e3bf0e56f18faa8664a6c9b4
Aaron Sherrill <aaron@codecommando.net>
2012-05-07 19:14:48 -0400

    Added some comments in app.js comparing AS3 to JavaScript

--- Resources/app.js
+++ Resources/app.js
@@ -3,6 +3,15 @@ require('lib/require_patch').monkeypatch(this);
 var Window = require('lib/Window');
 
 /*
+ * in Actionscript a new class instance looks like this
+ * 
+ * import flash.display.MovieClip;
+ * var my_mc:MovieClip = new MovieClip();
+ * 
+ * in Titanium it would like this
+ * 
+ * var MovieClip = require('flash/display/MovieClip');
+ * var my_mc = new MovieClip();
  * 
  */
 var mainWindow = new Window({
